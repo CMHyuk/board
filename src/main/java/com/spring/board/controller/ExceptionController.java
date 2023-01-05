9a7c31393bec61fdc785c6cc8ae71be9debc370d
CMHyuk <alsgur9710@naver.com>
2023-01-05 20:35:46 +0900

    feat : ExceptionController

---
+++ src/main/java/com/spring/board/controller/ExceptionController.java
@@ -0,0 +1,26 @@
+package com.spring.board.controller;
+
+import com.spring.board.exception.Exception;
+import com.spring.board.response.ErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> exception(Exception e) {
+        int statusCode = e.getStatus();
+
+        ErrorResponse body = ErrorResponse.builder()
+                .code(String.valueOf(statusCode))
+                .message(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(statusCode)
+                .body(body);
+    }
+}
