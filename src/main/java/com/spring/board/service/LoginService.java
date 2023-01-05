5bcafa2a03467c5faabd3c7401bbe3c363f9648d
CMHyuk <alsgur9710@naver.com>
2023-01-05 20:29:13 +0900

    feat : 로그인 로직

---
+++ src/main/java/com/spring/board/service/LoginService.java
@@ -0,0 +1,20 @@
+package com.spring.board.service;
+
+import com.spring.board.domain.User;
+import com.spring.board.exception.UserNotFound;
+import com.spring.board.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final UserRepository userRepository;
+
+    public User login(String loginId, String password) {
+        return userRepository.findByLoginId(loginId)
+                .filter(u -> u.getPassword().equals(password))
+                .orElseThrow(UserNotFound::new);
+    }
+}
