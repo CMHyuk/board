6a0224ec62739319e9150d8e7260f8816c3d7da3
CMHyuk <alsgur9710@naver.com>
2023-01-05 20:26:37 +0900

    feat : UserRepository

---
+++ src/main/java/com/spring/board/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.spring.board.repository;
+
+import com.spring.board.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLoginId(String loginId);
+}
