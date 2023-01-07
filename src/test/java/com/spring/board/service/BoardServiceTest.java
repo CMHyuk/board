671810f60e8bb59daa05ff08a66a1abdd05df832
CMHyuk <alsgur9710@naver.com>
2023-01-07 15:40:04 +0900

    test : 게시글 로직 테스트

---
+++ src/test/java/com/spring/board/service/BoardServiceTest.java
@@ -0,0 +1,228 @@
+package com.spring.board.service;
+
+import com.spring.board.domain.Board;
+import com.spring.board.domain.User;
+import com.spring.board.exception.BoardNotFound;
+import com.spring.board.exception.InvalidRequest;
+import com.spring.board.repository.BoardRepository;
+import com.spring.board.repository.UserRepository;
+import com.spring.board.request.board.EditBoardRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class BoardServiceTest {
+
+    @Autowired
+    BoardService boardService;
+
+    @Autowired
+    BoardRepository boardRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @BeforeEach
+    void clean() {
+        boardRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("게시글 저장 테스트")
+    void writeTest() {
+        //given
+        User user = User.builder()
+                .nickname("닉네임")
+                .loginId("아이디")
+                .password("비밀번호")
+                .build();
+
+        userRepository.save(user);
+
+        Board board = Board.builder()
+                .title("제목")
+                .content("내용")
+                .user(user)
+                .build();
+
+        //when
+        Board savedBoard = boardService.write(board);
+
+        //then
+        assertEquals(savedBoard.getTitle(), "제목");
+        assertEquals(savedBoard.getContent(), "내용");
+        assertEquals(savedBoard.getUser(), user);
+    }
+
+    @Test
+    @DisplayName("게시글 수정 테스트")
+    void editBoardTest() {
+        //given
+        User user = User.builder()
+                .nickname("닉네임")
+                .loginId("아이디")
+                .password("비밀번호")
+                .build();
+
+        userRepository.save(user);
+
+        Board board = Board.builder()
+                .title("제목")
+                .content("내용")
+                .user(user)
+                .build();
+
+        Board saveBoard = boardRepository.save(board);
+
+        EditBoardRequest request = new EditBoardRequest();
+        request.setTitle("제목수정");
+        request.setContent("내용수정");
+
+        //when
+        Board editBoard = boardService.editBoard(saveBoard.getId(), request, user);
+
+        //then
+        assertEquals(editBoard.getTitle(), "제목수정");
+        assertEquals(editBoard.getContent(), "내용수정");
+    }
+
+    @Test
+    @DisplayName("게시글 수정 실패 테스트")
+    void editBoardFailTest() {
+        //given
+        User user1 = User.builder()
+                .nickname("닉네임")
+                .loginId("아이디")
+                .password("비밀번호")
+                .build();
+
+        User user2 = User.builder()
+                .nickname("닉네임")
+                .loginId("아이디")
+                .password("비밀번호")
+                .build();
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        Board board = Board.builder()
+                .title("제목")
+                .content("내용")
+                .user(user1)
+                .build();
+
+        Board saveBoard = boardRepository.save(board);
+
+        EditBoardRequest request = new EditBoardRequest();
+        request.setTitle("제목수정");
+        request.setContent("내용수정");
+
+        //expected
+        assertThatThrownBy(() -> boardService.editBoard(saveBoard.getId(), request, user2))
+                .isInstanceOf(InvalidRequest.class);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 게시글 수정 테스트")
+    void editNonExistBoardTest() {
+        //given
+        User user = User.builder()
+                .nickname("닉네임")
+                .loginId("아이디")
+                .password("비밀번호")
+                .build();
+
+        userRepository.save(user);
+
+        EditBoardRequest request = new EditBoardRequest();
+        request.setTitle("제목수정");
+        request.setContent("내용수정");
+
+        //expected
+        assertThatThrownBy(() -> boardService.editBoard(100L, request, user))
+                .isInstanceOf(BoardNotFound.class);
+    }
+
+    @Test
+    @DisplayName("게시글 삭제 테스트")
+    void deleteBoardTest() {
+        //given
+        User user = User.builder()
+                .nickname("닉네임")
+                .loginId("아이디")
+                .password("비밀번호")
+                .build();
+
+        userRepository.save(user);
+
+        Board board = Board.builder()
+                .title("제목")
+                .content("내용")
+                .user(user)
+                .build();
+
+        Board saveBoard = boardRepository.save(board);
+
+        //when
+        boardService.deleteBoard(saveBoard.getId(), user);
+
+        //then
+        assertEquals(0, boardRepository.count());
+    }
+
+    @Test
+    @DisplayName("게시글 삭제 실패 테스트")
+    void deleteBoardFailTest() {
+        //given
+        User user1 = User.builder()
+                .nickname("닉네임")
+                .loginId("아이디")
+                .password("비밀번호")
+                .build();
+
+        User user2 = User.builder()
+                .nickname("닉네임")
+                .loginId("아이디")
+                .password("비밀번호")
+                .build();
+
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        Board board = Board.builder()
+                .title("제목")
+                .content("내용")
+                .user(user1)
+                .build();
+
+        Board saveBoard = boardRepository.save(board);
+
+        //expected
+        assertThatThrownBy(() -> boardService.deleteBoard(saveBoard.getId(), user2))
+                .isInstanceOf(InvalidRequest.class);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 게시글 삭제 테스트")
+    void deleteNonExistTest() {
+        //given
+        User user = User.builder()
+                .nickname("닉네임")
+                .loginId("아이디")
+                .password("비밀번호")
+                .build();
+
+        userRepository.save(user);
+
+        //expected
+        assertThatThrownBy(() -> boardService.deleteBoard(100L, user))
+                .isInstanceOf(BoardNotFound.class);
+    }
+}
