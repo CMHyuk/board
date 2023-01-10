f9a3e01eaf334e85cb012acf68b81684b4b3077e
CMHyuk <alsgur9710@naver.com>
2023-01-10 16:03:13 +0900

    feat : Reply 엔티티

---
+++ src/main/java/com/spring/board/domain/Reply.java
@@ -0,0 +1,62 @@
+package com.spring.board.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+import static javax.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Reply {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "reply_id")
+    private Long id;
+
+    private String reply;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @Builder
+    public Reply(String reply, User user, Board board, Comment comment) {
+        this.reply = reply;
+        this.user = user;
+        this.board = board;
+        this.comment = comment;
+        user.getReplies().add(this);
+        board.getReplies().add(this);
+        comment.getReplies().add(this);
+    }
+
+    public void UpdateReply(String reply) {
+        this.reply = reply;
+    }
+}
