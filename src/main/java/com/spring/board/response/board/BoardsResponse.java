314710723b227c9d5de619b39e6de49c44093508
CMHyuk <alsgur9710@naver.com>
2023-01-10 16:06:22 +0900

    refactor : @RequiredArgsConstructor -> @Builder로 변경

--- src/main/java/com/spring/board/response/board/BoardsResponse.java
+++ src/main/java/com/spring/board/response/board/BoardsResponse.java
@@ -1,15 +1,26 @@
 package com.spring.board.response.board;
 
+import com.spring.board.response.comment.CommentDto;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @Getter
-@RequiredArgsConstructor
 public class BoardsResponse {
 
     private final String userNickname;
     private final Long boardId;
     private final String title;
     private final String content;
+    private final List<CommentDto> comments;
 
+    @Builder
+    public BoardsResponse(String userNickname, Long boardId, String title, String content, List<CommentDto> comments) {
+        this.userNickname = userNickname;
+        this.boardId = boardId;
+        this.title = title;
+        this.content = content;
+        this.comments = comments;
+    }
 }
