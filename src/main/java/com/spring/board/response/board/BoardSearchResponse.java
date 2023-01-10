541cc049aeac54270f8bb288e40433456ddf0edf
CMHyuk <alsgur9710@naver.com>
2023-01-10 16:06:56 +0900

    refactor : 댓글 대댓글 추가, @RequiredArgsConstructor -> @Builder로 변경

--- src/main/java/com/spring/board/response/board/BoardSearchResponse.java
+++ src/main/java/com/spring/board/response/board/BoardSearchResponse.java
@@ -1,15 +1,30 @@
 package com.spring.board.response.board;
 
+import com.spring.board.response.comment.CommentDto;
+import com.spring.board.response.reply.ReplyDto;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @Getter
-@RequiredArgsConstructor
 public class BoardSearchResponse {
 
     private final String userNickname;
     private final Long boardId;
     private final String title;
     private final String content;
+    private final List<CommentDto> comments;
+    private final List<ReplyDto> replies;
 
+    @Builder
+    public BoardSearchResponse(String userNickname, Long boardId, String title, String content,
+                               List<CommentDto> comments, List<ReplyDto> replies) {
+        this.userNickname = userNickname;
+        this.boardId = boardId;
+        this.title = title;
+        this.content = content;
+        this.comments = comments;
+        this.replies = replies;
+    }
 }
