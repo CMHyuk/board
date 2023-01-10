9520d2e01e33caaf0836f3b98f428b12c4b4cb67
CMHyuk <alsgur9710@naver.com>
2023-01-10 16:01:04 +0900

    feat : 페치 조인으로 Comment 가져오는 메서드

--- src/main/java/com/spring/board/repository/CommentRepository.java
+++ src/main/java/com/spring/board/repository/CommentRepository.java
@@ -2,6 +2,14 @@ package com.spring.board.repository;
 
 import com.spring.board.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query(value = "select c from Comment c join fetch c.user join fetch c.board where c.id =:id")
+    Optional<Comment> findComment(@Param("id") Long id);
+
 }
