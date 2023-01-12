78db8f79672ca03be628f96ce324a1e3b5652b8c
CMHyuk <alsgur9710@naver.com>
2023-01-12 13:38:03 +0900

    feat : 게시글 좋아요 비즈니스 로직

---
+++ src/main/java/com/spring/board/service/LikeService.java
@@ -0,0 +1,65 @@
+package com.spring.board.service;
+
+import com.spring.board.domain.Board;
+import com.spring.board.domain.Like;
+import com.spring.board.domain.User;
+import com.spring.board.exception.BoardNotFound;
+import com.spring.board.exception.LikeNotFound;
+import com.spring.board.exception.UserNotFound;
+import com.spring.board.repository.BoardRepository;
+import com.spring.board.repository.LikeRepository;
+import com.spring.board.repository.UserRepository;
+import com.spring.board.response.like.LikeResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LikeService {
+
+    private final LikeRepository likeRepository;
+    private final BoardRepository boardRepository;
+    private final UserRepository userRepository;
+
+    public LikeResponse like(Long boardId, Long userId) {
+        User findUser = userRepository.findById(userId)
+                .orElseThrow(UserNotFound::new);
+
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(BoardNotFound::new);
+
+        Optional<Like> findLike = likeRepository.findByBoardIdAndUser(boardId, findUser);
+
+        if (findLike.isEmpty()) {
+            Like like = Like.builder()
+                    .board(board)
+                    .user(findUser)
+                    .build();
+            likeRepository.save(like);
+        }
+
+        return LikeResponse.builder()
+                .userNickname(findUser.getNickname())
+                .boardUserNickname(board.getUser().getNickname())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .build();
+    }
+
+    public void cancelLike(Long boardId, Long userId) {
+        User findUser = userRepository.findById(userId)
+                .orElseThrow(UserNotFound::new);
+
+        boardRepository.findById(boardId)
+                .orElseThrow(BoardNotFound::new);
+
+        Like findLike = likeRepository.findByBoardIdAndUser(boardId, findUser)
+                .orElseThrow(LikeNotFound::new);
+
+        likeRepository.delete(findLike);
+    }
+}
