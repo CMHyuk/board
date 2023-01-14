31c4f89260cf336cefcb3f075f48271ea1c0b20d
CMHyuk <alsgur9710@naver.com>
2023-01-14 12:47:22 +0900

    refactor : 페이징 파라미터 추가

--- src/main/java/com/spring/board/repository/BoardRepository.java
+++ src/main/java/com/spring/board/repository/BoardRepository.java
@@ -1,12 +1,13 @@
 package com.spring.board.repository;
 
 import com.spring.board.domain.Board;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    List<Board> findByTitleContaining(String title);
+    List<Board> findByTitleContaining(String title, Pageable pageable);
 
 }
