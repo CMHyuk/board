340ca60bcc4532bd38a50fc5d78b6a62928a8df0
CMHyuk <alsgur9710@naver.com>
2023-01-14 12:46:59 +0900

    refactor : dto 변경으로 인한 코드 변경, 페이징 추가

--- src/main/java/com/spring/board/service/BoardService.java
+++ src/main/java/com/spring/board/service/BoardService.java
@@ -2,17 +2,21 @@ package com.spring.board.service;
 
 import com.spring.board.domain.Board;
 import com.spring.board.domain.User;
-import com.spring.board.exception.board.BoardNotFound;
 import com.spring.board.exception.InvalidRequest;
+import com.spring.board.exception.board.BoardNotFound;
 import com.spring.board.exception.user.UserNotFound;
 import com.spring.board.repository.BoardRepository;
 import com.spring.board.repository.UserRepository;
 import com.spring.board.request.board.EditBoardRequest;
 import com.spring.board.request.board.WriteBoardRequest;
-import com.spring.board.response.board.*;
+import com.spring.board.response.board.BoardResponse;
+import com.spring.board.response.board.BoardsResponse;
+import com.spring.board.response.board.EditBoardResponse;
+import com.spring.board.response.board.WriteBoardResponse;
 import com.spring.board.response.comment.CommentDto;
 import com.spring.board.response.reply.ReplyDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -55,31 +59,32 @@ public class BoardService {
                 .boardId(board.getId())
                 .title(board.getTitle())
                 .content(board.getContent())
+                .comments(board.getComments().stream()
+                        .map(c -> new CommentDto(c.getComment(), c.getReplies().stream()
+                                .map(r -> new ReplyDto(r.getReply()))
+                                .collect(toList())))
+                        .collect(toList()))
                 .build();
     }
 
-    public List<BoardsResponse> getBoards() {
-        List<Board> boards = boardRepository.findAll();
-        return getBoardsResponses(boards);
-    }
-
-    public List<BoardsResponse> findBySearch(String title) {
-        List<Board> boards = boardRepository.findByTitleContaining(title);
-        return getBoardsResponses(boards);
-    }
-
-    private List<BoardsResponse> getBoardsResponses(List<Board> boards) {
-        return boards.stream()
+    public List<BoardsResponse> getBoards(Pageable pageable) {
+        return boardRepository.findAll(pageable).stream()
                 .map(b -> BoardsResponse.builder()
                         .boardId(b.getId())
                         .userNickname(b.getUser().getNickname())
                         .title(b.getTitle())
                         .content(b.getContent())
-                        .comments(b.getComments().stream()
-                                .map(c -> new CommentDto(c.getComment(), c.getReplies().stream()
-                                        .map(r -> new ReplyDto(r.getReply()))
-                                        .collect(toList())))
-                                .collect(toList()))
+                        .build())
+                .collect(toList());
+    }
+
+    public List<BoardsResponse> findBySearch(String title, Pageable pageable) {
+        return boardRepository.findByTitleContaining(title, pageable).stream()
+                .map(b -> BoardsResponse.builder()
+                        .boardId(b.getId())
+                        .userNickname(b.getUser().getNickname())
+                        .title(b.getTitle())
+                        .content(b.getContent())
                         .build())
                 .collect(toList());
     }
