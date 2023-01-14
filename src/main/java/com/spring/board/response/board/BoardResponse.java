b396f346c854460e9027b05563797d8b1ad0bece
CMHyuk <alsgur9710@naver.com>
2023-01-14 12:46:14 +0900

    feat : 댓글 반환 추가

--- src/main/java/com/spring/board/response/board/BoardResponse.java
+++ src/main/java/com/spring/board/response/board/BoardResponse.java
@@ -1,7 +1,10 @@
 package com.spring.board.response.board;
 
+import com.spring.board.response.comment.CommentDto;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 public class BoardResponse {
@@ -10,12 +13,14 @@ public class BoardResponse {
     private final Long boardId;
     private final String title;
     private final String content;
+    private final List<CommentDto> comments;
 
     @Builder
-    public BoardResponse(String nickname, Long boardId, String title, String content) {
+    public BoardResponse(String nickname, Long boardId, String title, String content, List<CommentDto> comments) {
         this.nickname = nickname;
         this.boardId = boardId;
         this.title = title;
         this.content = content;
+        this.comments = comments;
     }
 }
