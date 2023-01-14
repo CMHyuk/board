99386c266b22ff4f29b32cc4751ba17926cf5f2f
CMHyuk <alsgur9710@naver.com>
2023-01-14 12:47:47 +0900

    refactor : 페이징 추가로 인한 테스트 코드 수정

--- src/test/java/com/spring/board/service/BoardServiceTest.java
+++ src/test/java/com/spring/board/service/BoardServiceTest.java
@@ -2,12 +2,13 @@ package com.spring.board.service;
 
 import com.spring.board.domain.Board;
 import com.spring.board.domain.User;
-import com.spring.board.exception.board.BoardNotFound;
 import com.spring.board.exception.InvalidRequest;
+import com.spring.board.exception.board.BoardNotFound;
 import com.spring.board.repository.BoardRepository;
 import com.spring.board.repository.UserRepository;
 import com.spring.board.request.board.EditBoardRequest;
 import com.spring.board.request.board.WriteBoardRequest;
+import com.spring.board.response.board.BoardsResponse;
 import com.spring.board.response.board.EditBoardResponse;
 import com.spring.board.response.board.WriteBoardResponse;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,10 +16,17 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @SpringBootTest
 class BoardServiceTest {
@@ -226,5 +234,35 @@ class BoardServiceTest {
         //expected
         assertThatThrownBy(() -> boardService.deleteBoard(100L, user))
                 .isInstanceOf(BoardNotFound.class);
+    }
+
+    @Test
+    @DisplayName("게시글 전체 조회")
+    void getBoardsTest() {
+        //given
+        User user = User.builder()
+                .nickname("닉네임")
+                .loginId("아이디")
+                .password("비밀번호")
+                .build();
+
+        userRepository.save(user);
+
+        List<Board> boards = IntStream.range(0, 20).mapToObj(i -> Board.builder()
+                        .title("제목" + i)
+                        .content("내용" + i)
+                        .user(user)
+                        .build())
+                .collect(Collectors.toList());
+
+        boardRepository.saveAll(boards);
+
+        //when
+        Pageable pageable = PageRequest.of(0, 10, DESC, "id");
+        List<BoardsResponse> findBoards = boardService.getBoards(pageable);
+
+        //then
+        assertEquals(findBoards.get(0).getTitle(), "제목19");
+        assertEquals(findBoards.get(9).getTitle(), "제목10");
     }
 }
