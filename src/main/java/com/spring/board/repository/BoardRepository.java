37ca3d4cb00cffbbe94b87d4f20390d2b8cbce86
CMHyuk <alsgur9710@naver.com>
2023-01-17 20:56:15 +0900

    feat : 쿼리 추가

--- src/main/java/com/spring/board/repository/BoardRepository.java
+++ src/main/java/com/spring/board/repository/BoardRepository.java
@@ -3,11 +3,21 @@ package com.spring.board.repository;
 import com.spring.board.domain.Board;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
-    List<Board> findByTitleContaining(String title, Pageable pageable);
+    @Query(value = "select b from Board b join fetch b.user where b.title like %:title%")
+    List<Board> findByTitleContaining(@Param("title") String title, Pageable pageable);
+
+    @Query(value = "select b from Board b join fetch b.user")
+    List<Board> findBoardWithUser(Pageable pageable);
+
+    @Query(value = "select b from Board b join fetch b.user left join fetch b.comments where b.id =:id")
+    Optional<Board> findWithAll(@Param("id") Long id);
 
 }
