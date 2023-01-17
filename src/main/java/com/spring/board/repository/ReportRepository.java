310f06f59c5d80e1e99aa7153651a55211d0979e
CMHyuk <alsgur9710@naver.com>
2023-01-17 21:02:01 +0900

    feat : 쿼리 추가

--- src/main/java/com/spring/board/repository/ReportRepository.java
+++ src/main/java/com/spring/board/repository/ReportRepository.java
@@ -2,11 +2,16 @@ package com.spring.board.repository;
 
 import com.spring.board.domain.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
     Optional<Report> findByBoardIdAndUserId(Long boardId, Long userId);
+
+    @Query(value = "select r from Report r join fetch r.board")
+    List<Report> findWithBoard();
 
 }
