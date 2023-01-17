510ce5a6d665cb86709ce8dafbbf9eb4914d153a
CMHyuk <alsgur9710@naver.com>
2023-01-17 20:58:56 +0900

    feat : 쿼리 추가

--- src/main/java/com/spring/board/repository/UserRepository.java
+++ src/main/java/com/spring/board/repository/UserRepository.java
@@ -2,10 +2,16 @@ package com.spring.board.repository;
 
 import com.spring.board.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByLoginId(String loginId);
+
+    @Query(value = "select u from User u join fetch u.boards where u.id =:id")
+    Optional<User> findUserWithBoards(@Param("id") Long id);
+
 }
