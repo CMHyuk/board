06b0ea6eaa19dfda99db7570973c662ad2926dbd
CMHyuk <alsgur9710@naver.com>
2023-01-19 20:01:33 +0900

    refactor : 대댓글 fetch join 제거 후 메서드 명 변경

--- src/main/java/com/spring/board/repository/CommentRepository.java
+++ src/main/java/com/spring/board/repository/CommentRepository.java
@@ -1,7 +1,6 @@
 package com.spring.board.repository;
 
 import com.spring.board.domain.Comment;
-import com.spring.board.domain.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,9 +10,8 @@ import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query(value = "select c from Comment c join fetch c.user join fetch c.board " +
-            "left join fetch c.replies where c.id =:id")
-    Optional<Comment> findComment(@Param("id") Long id);
+    @Query(value = "select c from Comment c join fetch c.user join fetch c.board where c.id =:id")
+    Optional<Comment> findCommentWithUserAndBoard(@Param("id") Long id);
 
     List<Comment> findByBoardId(Long id);
 }
