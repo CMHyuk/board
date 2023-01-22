4c77a2d50c38041435589ed35dff94a90a3d6877
CMHyuk <alsgur9710@naver.com>
2023-01-22 21:32:09 +0900

    feat : 검증 추가

--- src/main/java/com/spring/board/request/comment/EditCommentRequest.java
+++ src/main/java/com/spring/board/request/comment/EditCommentRequest.java
@@ -3,8 +3,12 @@ package com.spring.board.request.comment;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class EditCommentRequest {
+
+    @NotBlank(message = "댓글을 입력하세요.")
     private String comment;
 }
--- src/main/java/com/spring/board/request/reply/EditReplyRequest.java
+++ src/main/java/com/spring/board/request/reply/EditReplyRequest.java
@@ -3,8 +3,12 @@ package com.spring.board.request.reply;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class EditReplyRequest {
+
+    @NotBlank(message = "대댓글을 입력하세요.")
     private String reply;
 }
