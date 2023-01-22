2c9977405d8b1fc92c8bb6a8acb82de7c0584729
CMHyuk <alsgur9710@naver.com>
2023-01-22 21:31:19 +0900

    feat : 검증 추가

--- src/main/java/com/spring/board/request/ReportRequest.java
+++ src/main/java/com/spring/board/request/ReportRequest.java
@@ -3,8 +3,12 @@ package com.spring.board.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class ReportRequest {
+
+    @NotBlank(message = "신고 내용을 입력하세요.")
     private String reportContent;
 }
