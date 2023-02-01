8fdf9d7c35a5dbcc62312e8aa1cb41e8e9d2fc04
CMHyuk <alsgur9710@naver.com>
2023-02-01 21:24:19 +0900

    feat : ExceptionHandler 추가

--- src/main/java/com/spring/board/controller/ExceptionController.java
+++ src/main/java/com/spring/board/controller/ExceptionController.java
@@ -1,16 +1,23 @@
 package com.spring.board.controller;
 
 import com.spring.board.exception.Exception;
+import com.spring.board.response.BindErrorResponse;
 import com.spring.board.response.ErrorResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@RestControllerAdvice
 public class ExceptionController {
 
-    @ResponseBody
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> exception(Exception e) {
         int statusCode = e.getStatus();
@@ -22,5 +29,17 @@ public class ExceptionController {
 
         return ResponseEntity.status(statusCode)
                 .body(body);
+    }
+
+    @ExceptionHandler(BindException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public BindErrorResponse BindException(BindException bindException) {
+        List<FieldError> errors = bindException.getFieldErrors();
+
+        return BindErrorResponse.builder()
+                .message(errors.stream()
+                        .map(e -> e.getDefaultMessage())
+                        .collect(Collectors.toList()))
+                .build();
     }
 }
