01ed01bcb6529b9e724796fd9fd2f8d517264f44
CMHyuk <alsgur9710@naver.com>
2023-02-01 21:22:07 +0900

    feat : 신고한 회원 닉네임 추가

--- src/main/java/com/spring/board/response/report/ReportBoardsResponse.java
+++ src/main/java/com/spring/board/response/report/ReportBoardsResponse.java
@@ -7,14 +7,16 @@ import lombok.Getter;
 public class ReportBoardsResponse {
 
     private Long reportId;
+    private String reporter;
     private String reportContent;
     private Long boardId;
     private String title;
     private String content;
 
     @Builder
-    public ReportBoardsResponse(Long reportId, String reportContent, Long boardId, String title, String content) {
+    public ReportBoardsResponse(Long reportId, String reporter, String reportContent, Long boardId, String title, String content) {
         this.reportId = reportId;
+        this.reporter = reporter;
         this.reportContent = reportContent;
         this.boardId = boardId;
         this.title = title;
