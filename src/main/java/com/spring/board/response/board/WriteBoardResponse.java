8995a4eb9f3fd8c29aff577f3e26e10a22962534
CMHyuk <alsgur9710@naver.com>
2023-02-01 21:20:58 +0900

    feat : 회원 닉네임, 등급 반환 추가

--- src/main/java/com/spring/board/response/board/WriteBoardResponse.java
+++ src/main/java/com/spring/board/response/board/WriteBoardResponse.java
@@ -1,16 +1,21 @@
 package com.spring.board.response.board;
 
+import com.spring.board.domain.Grade;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class WriteBoardResponse {
 
+    private final String nickname;
+    private final Grade grade;
     private final String title;
     private final String content;
 
     @Builder
-    public WriteBoardResponse(String title, String content) {
+    public WriteBoardResponse(String nickname, Grade grade, String title, String content) {
+        this.nickname = nickname;
+        this.grade = grade;
         this.title = title;
         this.content = content;
     }
