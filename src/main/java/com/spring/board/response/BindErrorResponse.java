8af7c041aba1308362f73c9dfd821ca4c5b6576d
CMHyuk <alsgur9710@naver.com>
2023-02-01 21:23:28 +0900

    feat : BindingResult 반환을 위한 클래스

---
+++ src/main/java/com/spring/board/response/BindErrorResponse.java
@@ -0,0 +1,17 @@
+package com.spring.board.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class BindErrorResponse {
+
+    private final List<String> message;
+
+    @Builder
+    public BindErrorResponse(List<String> message) {
+        this.message = message;
+    }
+}
