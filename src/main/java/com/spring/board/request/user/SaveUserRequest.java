201ccbb659d3210eab8668697ea5dd9c4bddf247
CMHyuk <alsgur9710@naver.com>
2023-02-01 21:21:22 +0900

    feat : 회원가입 검증 추가

--- src/main/java/com/spring/board/request/user/SaveUserRequest.java
+++ src/main/java/com/spring/board/request/user/SaveUserRequest.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
@@ -13,9 +14,13 @@ public class SaveUserRequest {
     private String nickname;
 
     @NotBlank(message = "아이디를 입력하세요.")
+    @Pattern(regexp = "^[a-zA-Z]{1}[a-zA-Z0-9]{8,12}$",
+            message = "아이디는 영문 + 숫자 조합으로 8 ~ 12자리를 입력해주세요.")
     private String loginId;
 
     @NotBlank(message = "비밀번호를 입력하세요.")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[~`!@#$%\\^&*()-])(?=.*[a-z]).{8,16}$",
+            message = "비밀번호는 영문 + 숫자 조합으로 8 ~ 16자리를 입력해주세요.")
     private String password;
 
 }
