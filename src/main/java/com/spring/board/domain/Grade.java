218c76ca68de78c69a6e24397953e51107f383ad
CMHyuk <alsgur9710@naver.com>
2023-02-07 12:13:19 +0900

    refactor : Lombok 사용으로 변경

--- src/main/java/com/spring/board/domain/Grade.java
+++ src/main/java/com/spring/board/domain/Grade.java
@@ -1,18 +1,15 @@
 package com.spring.board.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public enum Grade {
     SILVER, GOLD(5), RED(10), ADMIN;
 
     private int requiredBoardCount;
 
-    Grade(int requiredBoardCount) {
-        this.requiredBoardCount = requiredBoardCount;
-    }
-
-    Grade() {
-    }
-
-    public int getRequiredBoardCount() {
-        return requiredBoardCount;
-    }
 }
