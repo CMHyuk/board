6d5f593fd69a2d13384eec0ae7e81c92237541f6
CMHyuk <alsgur9710@naver.com>
2023-02-09 18:56:40 +0900

    feat : 회원 id로 신고 게시글 찾기

--- src/main/java/com/spring/board/repository/ReportRepository.java
+++ src/main/java/com/spring/board/repository/ReportRepository.java
@@ -14,6 +14,8 @@ public interface ReportRepository extends JpaRepository<Report, Long> {
 
     List<Report> findByBoardId(Long boardId);
 
+    List<Report> findByUserId(Long userId);
+
     @Query(value = "select r from Report r join fetch r.board")
     List<Report> findWithBoard(Pageable pageable);
 
